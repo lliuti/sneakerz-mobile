f8505799599eb04c830b3c0074aaf28fdd7d92a9
lliuti <dev.lliuti@gmail.com>
2020-02-13 21:19:17 -0300

    Starting cart page

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import {} from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons/';
 
-import { Container } from './styles';
+import { 
+  Container,
+  Product,
+  ProductThumbnail,
+  ProductDescription,
+  ProductName,
+  ProductPrice,
+  ProductAmount,
+  Buttons,
+  AddButton,
+  SubtractButton
+} from './styles';
 
 export default function Cart() {
   return (
-    <Container></Container>
+    <Container>
+      <Product>
+        <ProductThumbnail source={{ uri: 'https://cdn.webshopapp.com/shops/186401/files/284556393/asics-asics-jolt-2-ladies-running-shoes-2019.jpg' }}/>
+        <ProductDescription>
+          <ProductName>Asics Jolt</ProductName>
+          <ProductPrice>R$ 215,90</ProductPrice>
+        </ProductDescription>
+        <ProductAmount>
+          1
+        </ProductAmount>
+        <Buttons>
+          <AddButton>
+            <MaterialIcons name='add' color='#00ff00' size={36}/>
+          </AddButton>
+          <SubtractButton>
+            <MaterialIcons name='remove' color='#ff0000' size={36}/>
+          </SubtractButton>
+        </Buttons>
+      </Product>
+    </Container>
   );
 }
--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -1,5 +1,51 @@
+import { StatusBar } from 'react-native';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
+  flex: 1;
+  margin-top: ${StatusBar.currentHeight}px;
+  padding: 20px;
+`;
+
+export const Product = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
   background: #333;
+  padding: 10px 05px;
+  margin-bottom: 10px;
+`;
+export const ProductThumbnail = styled.Image`
+  width: 80px;
+  height: 80px;
+  align-items: center;
+  justify-content: center;
+`;
+export const ProductDescription = styled.View`
+  align-items: center;
+  justify-content: center;
+`;
+export const ProductName = styled.Text`
+  color: #f5f5f5;
+  font-size: 22px;
+`;
+export const ProductPrice = styled.Text`
+  color: #ddd;
+  font-size: 18px;
+`;
+export const ProductAmount = styled.Text`
+  color: #f5f5f5;
+  font-size: 22px;
+`;
+export const Buttons = styled.View`
+  align-items: center;
+  justify-content: center;
+`;
+export const AddButton = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;  
+`;
+export const SubtractButton = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
 `;
