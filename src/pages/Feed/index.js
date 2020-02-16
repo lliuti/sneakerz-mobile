c40fe02b0e8ccaff9701f3c52d605e4362d89578
lliuti <dev.lliuti@gmail.com>
2020-02-15 23:26:50 -0300

    adding navigate function to all (mocked) products

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -43,7 +43,9 @@ export default function Feed({ navigation }) {
               <MaterialIcons name='shopping-cart' color='#333' size={24}/>
             </AddToCart> */}
           </Product>
-          <Product>
+          <Product onPress={() =>
+            navigation.navigate('Product')
+          }>
             <ProductTitle>Generic Jordan</ProductTitle>
             <ProductPrice>R$ 655,90</ProductPrice>
             <Thumbnail source={{ uri: 'https://i.ebayimg.com/images/g/ra0AAOSw~HRdzwqy/s-l300.png' }}/>
@@ -58,7 +60,9 @@ export default function Feed({ navigation }) {
           <Category>Running</Category>
         </CategoryBox>
         <ProductGroup>
-          <Product>
+          <Product onPress={() =>
+            navigation.navigate('Product')
+          }>
             <ProductTitle>Asics Jolt</ProductTitle>
             <ProductPrice>R$ 215,90</ProductPrice>
             <Thumbnail source={{ uri: 'https://cdn.webshopapp.com/shops/186401/files/284556393/asics-asics-jolt-2-ladies-running-shoes-2019.jpg' }}/>
@@ -66,7 +70,9 @@ export default function Feed({ navigation }) {
               <MaterialIcons name='shopping-cart' color='#333' size={24}/>
             </AddToCart> */}
           </Product>
-          <Product>
+          <Product onPress={() =>
+            navigation.navigate('Product')
+          }>
             <ProductTitle>Nike Free RN</ProductTitle>
             <ProductPrice>R$ 385,90</ProductPrice>
             <Thumbnail source={{ uri: 'https://media1.popsugar-assets.com/files/thumbor/pDkt7AcMtKwn8X6179F6Zt60khI/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/04/16/709/n/1922729/tmp_GPDTlx_9f1a53cddc457fd4_AQ1316_100_E_PREM_86375.PNG' }}/>
@@ -81,7 +87,9 @@ export default function Feed({ navigation }) {
           <Category>Skate</Category>
         </CategoryBox>
         <ProductGroup>
-          <Product>
+          <Product onPress={() =>
+            navigation.navigate('Product')
+          }>
             <ProductTitle>Converse 76 Mid</ProductTitle>
             <ProductPrice>R$ 185,90</ProductPrice>
             <Thumbnail source={{ uri: 'https://i.ebayimg.com/images/g/AJ4AAOSw~ftdbYgz/s-l640.png' }}/>
@@ -89,7 +97,9 @@ export default function Feed({ navigation }) {
               <MaterialIcons name='shopping-cart' color='#333' size={24}/>
             </AddToCart> */}
           </Product>
-          <Product>
+          <Product onPress={() =>
+            navigation.navigate('Product')
+          }>
             <ProductTitle>Trikeis</ProductTitle>
             <ProductPrice>R$ 205,90</ProductPrice>
             <Thumbnail source={{ uri: 'https://i.pinimg.com/originals/ed/f2/d3/edf2d36c79acc7aa007cc4a7d03657ac.png' }}/>
@@ -97,7 +107,9 @@ export default function Feed({ navigation }) {
               <MaterialIcons name='shopping-cart' color='#333' size={24}/>
             </AddToCart> */}
           </Product>
-          <Product>
+          <Product onPress={() =>
+            navigation.navigate('Product')
+          }>
             <ProductTitle>DC</ProductTitle>
             <ProductPrice>R$ 185,90</ProductPrice>
             <Thumbnail source={{ uri: 'https://assets.xtechcommerce.com/uploads/images/medium/9255b194c627235f0e54b452e72e0ae9.png' }}/>
