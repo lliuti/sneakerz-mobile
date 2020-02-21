f80349757b76f55b6dbac280310f7e1f15102a97
lliuti <dev.lliuti@gmail.com>
2020-02-21 00:52:06 -0300

    Finishing Cart screen layout

--- src/screens/Cart/index.js
+++ src/screens/Cart/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MaterialIcons } from '@expo/vector-icons/';
 
 import { 
   Container,
+  ProductList,
   Product,
   ProductThumbnail,
   ProductDescription,
@@ -11,30 +12,65 @@ import {
   ProductAmount,
   Buttons,
   AddButton,
-  SubtractButton
+  SubtractButton,
+  FinishButton,
+  ButtonText,
+  TotalTitle,
+  TotalBox,
+  Total,
+  Bottom,
+  Wrapper,
 } from './styles';
 
 export default function Cart() {
+  const productPrice = 210;
+
+  const [amount, setAmount] = useState(1);
+  const [total, setTotal] = useState(productPrice);
+
+  function handleAdd() {
+    setAmount(amount + 1);
+    setTotal(amount == 0 ? productPrice : total + productPrice);
+  };
+
+  function handleSubtract() {
+    setAmount(amount == 0 ? 0 : amount - 1);
+    setTotal(amount <= 1 ? productPrice : total - productPrice);
+  };
+
   return (
     <Container>
-      <Product>
-        <ProductThumbnail source={{ uri: 'https://cdn.webshopapp.com/shops/186401/files/284556393/asics-asics-jolt-2-ladies-running-shoes-2019.jpg' }}/>
-        <ProductDescription>
-          <ProductName>Asics Jolt</ProductName>
-          <ProductPrice>R$ 215,90</ProductPrice>
-        </ProductDescription>
-        <ProductAmount>
-          1
-        </ProductAmount>
-        <Buttons>
-          <AddButton>
-            <MaterialIcons name='add' color='#00ff00' size={36}/>
-          </AddButton>
-          <SubtractButton>
-            <MaterialIcons name='remove' color='#ff0000' size={36}/>
-          </SubtractButton>
-        </Buttons>
-      </Product>
+      <ProductList>
+        <Product>
+          <ProductThumbnail source={{ uri: 'https://cdn.webshopapp.com/shops/186401/files/284556393/asics-asics-jolt-2-ladies-running-shoes-2019.jpg' }}/>
+          <ProductDescription>
+            <ProductName>Asics Jolt</ProductName>
+            <ProductPrice>R$ {productPrice}</ProductPrice>
+          </ProductDescription>
+          <ProductAmount>
+            {amount}
+          </ProductAmount>
+          <Buttons>
+            <AddButton onPress={handleAdd}>
+              <MaterialIcons name='add' color='#00ff00' size={36}/>
+            </AddButton>
+            <SubtractButton onPress={handleSubtract}>
+              <MaterialIcons name='remove' color='#ff0000' size={36}/>
+            </SubtractButton>
+          </Buttons>
+        </Product>
+      </ProductList>
+      <Bottom>
+        <TotalBox>
+          <TotalTitle>Total:</TotalTitle>
+          <Total>R$ {total == 0 ? productPrice : total}</Total>
+        </TotalBox>
+        <Wrapper>
+          <FinishButton>
+            <ButtonText>FINISH</ButtonText>
+          </FinishButton>
+        </Wrapper>
+      </Bottom>
     </Container>
   );
 }
--- src/screens/Cart/styles.js
+++ src/screens/Cart/styles.js
@@ -7,6 +7,10 @@ export const Container = styled.View`
   padding: 20px;
 `;
 
+export const ProductList = styled.ScrollView.attrs({
+
+})``;
+
 export const Product = styled.View`
   flex-direction: row;
   align-items: center;
@@ -48,4 +52,41 @@ export const AddButton = styled.TouchableOpacity`
 export const SubtractButton = styled.TouchableOpacity`
   align-items: center;
   justify-content: center;
+`;
+
+export const Bottom = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+export const Wrapper = styled.View`
+  justify-content: center;
+`;
+
+export const FinishButton = styled.TouchableOpacity`
+  background: #FFF41C;
+  padding: 10px 50px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 15;
+  elevation: 2;
+`;
+
+export const ButtonText = styled.Text`
+  color: #333;
+  font-size: 26px;
+`;
+
+export const TotalBox = styled.View`
+  margin: 15px 0;
+  align-items: flex-end;
+`;
+
+export const TotalTitle = styled.Text`
+  font-size: 22px;
+`;
+
+export const Total = styled.Text`
+  font-size: 26px;
+  color: #444;
 `;
